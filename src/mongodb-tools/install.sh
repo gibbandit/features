2e0606fa26f730f280ac620869208cf3bbecb2a9
gibbandit <james@hexiton.com>
2022-11-20 16:59:02 +0000

    change install

--- src/mongodb-tools/install.sh
+++ src/mongodb-tools/install.sh
@@ -13,7 +13,7 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-MONGO_TOOLS_AVAIL_VERSIONS=($(curl -sSlr GET "https://s3.amazonaws.com/repo.mongodb.org?list-type=2&prefix=apt/debian/dists/$VERSION_CODENAME/mongodb-org/&delimiter=/" |grep -oPu '(?<=mongodb-org\/)[0-9]*\.[0-9]*?(?=\/)'| sort -rnu))
+MONGO_TOOLS_AVAIL_VERSIONS=($(curl -sSlr GET "https://s3.amazonaws.com/repo.mongodb.org?list-type=2&prefix=apt/debian/dists/$VERSION_CODENAME/mongodb-org/&delimiter=/" | grep -oPu '(?<=mongodb-org\/)[0-9]*\.[0-9]*?(?=\/)' | sort -rnu))
 
 if [ "${MONGO_TOOLS_VERSION}" = "latest" ] || [ "${MONGO_TOOLS_VERSION}" = "current" ] || [ "${MONGO_TOOLS_VERSION}" = "lts" ]; then
     MONGO_TOOLS_VERSION=${MONGO_TOOLS_AVAIL_VERSIONS[0]}
@@ -25,23 +25,26 @@ fi
 
 architecture="$(uname -m)"
 case $architecture in
-    x86_64) architecture="amd64";;
-    aarch64 | armv8* | arm64) architecture="arm64";;
-    *) echo "(!) Architecture $architecture unsupported"; exit 1 ;;
+x86_64) architecture="amd64" ;;
+aarch64 | armv8* | arm64) architecture="arm64" ;;
+*)
+    echo "(!) Architecture $architecture unsupported"
+    exit 1
+    ;;
 esac
 
-curl -sSL "https://www.mongodb.org/static/pgp/server-${MONGO_TOOLS_VERSION}.asc" | gpg --dearmor > /usr/share/keyrings/mongodb-archive-keyring.gpg
+curl -sSL "https://www.mongodb.org/static/pgp/server-${MONGO_TOOLS_VERSION}.asc" | gpg --dearmor >/usr/share/keyrings/mongodb-archive-keyring.gpg
 echo "deb [arch=${architecture} signed-by=/usr/share/keyrings/mongodb-archive-keyring.gpg] http://repo.mongodb.org/apt/debian ${VERSION_CODENAME}/mongodb-org/${MONGO_TOOLS_VERSION} main" | tee /etc/apt/sources.list.d/mongodb-org-${MONGO_TOOLS_VERSION}.list
 
 apt-get update
 
-export DEBIAN_FRONTEND=noninteractive 
+export DEBIAN_FRONTEND=noninteractive
 
-apt-get install -y mongodb-mongosh 
+apt-get install -y mongodb-mongosh
 
-if [ ${architecture} = "amd64" ]; then 
-    apt-get install -y mongodb-database-tools; 
+if [ ${architecture} = "amd64" ]; then
+    apt-get install -y mongodb-database-tools
 fi
 
-apt-get clean -y 
+apt-get clean -y
 rm -rf /var/lib/apt/lists/*
